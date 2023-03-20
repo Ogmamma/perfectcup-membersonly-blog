ffd0b12663de83f6d3cdd2c024ef6b8f5219d7ac
Ogmamma <126420039+Ogmamma@users.noreply.github.com>
2023-03-20 01:26:26 +0100

    Add files via upload

---
+++ blog.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>The Perfect Cup - Blog</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/business-casual.css" rel="stylesheet">
+
+    <!-- Fonts -->
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div class="brand">The Perfect Cup</div>
+    <div class="address-bar">3481 Melrose Place | Beverly Hills, CA 90210 | 123.456.7890</div>
+
+    <!-- Navigation -->
+<?php require_once 'nav.php'; ?>
+
+    <div class="container">
+
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                    <hr>
+                    <h2 class="intro-text text-center">The
+                        <strong>Perfect Cup</strong>
+                    </h2>
+                    <hr>
+                </div>
+                <div class="col-lg-12 text-center">
+                    <img class="img-responsive img-border img-full" src="img/slide-1.jpg" alt="">
+                    <h2>Naruto zabe
+                        <br>
+                        <small>October 13, 2022</small>
+                    </h2>
+                    <p>Kad bi vi znali, sta se sve izdesavalo, odakle je sve to krenulo da se naruto pojavi u tom trenutku kakav crni attack on titan kakav bilo koji anime ako pričamo o ozbiljnim stvarima
+Kako se pojavi, sta nosi, sa kim se bije i kako se bije
+Je nešto sto nikad vise nece da se ponovi u anime svetu</p>
+                    <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Read More</button>
+                    <hr>
+                </div>
+                <div class="col-lg-12 text-center">
+                    <img class="img-responsive img-border img-full" src="img/slide-2.jpg" alt="">
+                    <h2>Irish coffee
+                        <br>
+                        <small>October 13, 2022</small>
+                    </h2>
+                    <p>Different variations of coffee cocktails pre-date the now-classic Irish coffee by at least 100 years.
+
+ From the mid-19th century, the Pharisäer and the Fiaker were served in Viennese coffee houses; both were coffee cocktails served in glass, topped with whipped cream.</p>
+                    <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal2">Read More</button>
+                    <hr>
+                </div>
+                <div class="col-lg-12 text-center">
+                    <img class="img-responsive img-border img-full" src="img/slide-3.jpg" alt="">
+                    <h2>Frozen Caramel Latte
+                        <br>
+                        <small>October 13, 2022</small>
+                    </h2>
+                    <p>The term comes from the Italian caffellatte or caffè latte, from caffè e latte, literally "coffee and milk"; in English orthography either or both words sometimes have an accent on the final e (a hyperforeignism in the case of *latté, or to indicate it is pronounced, not the more-common silent final e of English).</p>
+                    <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal3">Read More</button>
+                    <hr>
+                </div>
+                <div class="col-lg-12 text-center">
+                    <ul class="pager">
+                        <li class="previous"><a href="#">&larr; Older</a>
+                        </li>
+                        <li class="next"><a href="#">Newer &rarr;</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container -->
+    
+	<!--Modal 1-->
+	<div id="myModal" class="modal fade" role="dialog">
+	  <div class="modal-dialog">
+	
+    <!-- Modal content-->
+	<div class="modal-content">
+	<div class="modal-header">
+	  <button type="button" class="close" data-dismiss="modal">&times;</button>
+	    <h4 class="modal-title">Naruto Zabe</h4>
+	</div>
+	<div class="modal-body">
+	  <p>Sample Text</p>
+	  </div>
+	  <div class="modal-footer">
+	    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+	   </div>
+	   </div>
+	   </div>
+	   </div>
+	   
+	   	<!--Modal 2-->
+	<div id="myModal2" class="modal fade" role="dialog">
+	  <div class="modal-dialog">
+	
+    <!-- Modal content-->
+	<div class="modal-content">
+	<div class="modal-header">
+	  <button type="button" class="close" data-dismiss="modal">&times;</button>
+	    <h4 class="modal-title">Irish Coffee</h4>
+	</div>
+	<div class="modal-body">
+	  <p>Sample Text</p>
+	  </div>
+	  <div class="modal-footer">
+	    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+	   </div>
+	   </div>
+	   </div>
+	   </div>
+
+	<!--Modal 3-->
+	<div id="myModal3" class="modal fade" role="dialog">
+	  <div class="modal-dialog">
+	
+    <!-- Modal content-->
+	<div class="modal-content">
+	<div class="modal-header">
+	  <button type="button" class="close" data-dismiss="modal">&times;</button>
+	    <h4 class="modal-title">Frozen Caramel Latte</h4>
+	</div>
+	<div class="modal-body">
+	  <p>Sample Text</p>
+	  </div>
+	  <div class="modal-footer">
+	    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+	   </div>
+	   </div>
+	   </div>
+	   </div>
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <p>Copyright &copy; The Perfect Cup 2023</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
