53d4ddd9b5e03e98942e90e6211ff9e27cf5073d
Ogmamma <126420039+Ogmamma@users.noreply.github.com>
2023-05-28 01:19:53 +0200

    Update about.php

--- about.php
+++ about.php
@@ -50,7 +50,7 @@
                     <hr>
                 </div>
                 <div class="col-md-6">
-<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://www.youtube.com/embed/UQSM3M1I5y8' frameborder='0' allowfullscreen></iframe></div>
+<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='https://www.youtube.com/watch?v=gK_qiu9W7TU' frameborder='0' allowfullscreen></iframe></div>
                 </div>
                 <div class="col-md-6">
                     <p>The Perfect Cup launched in 2000, in the heart of California. Our mission is simple. We want to teach coffee drinkers all over the world how to make the perfect blend! Our coffee experts have a lifelong passion for coffee. They are internationally traveled and have experienced many varieties.</p>
