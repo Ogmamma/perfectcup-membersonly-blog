ae1a1691521ed8939bfb2eae7b27cf25e0b17687
Dimitrije <126420039+Ogmamma@users.noreply.github.com>
2023-07-29 21:18:48 +0200

    fixed password hashing

--- adduser.php
+++ adduser.php
@@ -26,10 +26,10 @@ if (strlen($fname) < 2) {
 } else { 
 
     //PASSWORD ENCRYPTION USING BCRYPT PHP METHOD
-    $spassword = password_hash($password, PASSWORD_BCRYPT, array('cost' => 12));
+    $password = password_hash($password, PASSWORD_BCRYPT, array('cost' => 12));
 	
 	$query = "SELECT * FROM members WHERE email='$email'";
-	$result = mysqli_query($mysqli, $query) or die (mysqli_error());
+	$result = mysqli_query($mysqli, $query) or die (mysqli_error($mysqli));
 	$num_row = mysqli_num_rows($result);
 	$row = mysqli_fetch_array($result);
 	
--- pcheck.php
+++ pcheck.php
@@ -5,14 +5,14 @@ session_start();
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-$mysqli = new mysqli('localhost', 'root', 'perfectcup');
+$mysqli = new mysqli('localhost', 'root', '', 'perfectcup');
 
 if ($mysqli->connect_error) {
 	die('Error: (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
 }
 
 $query = "SELECT * FROM members WHERE email='$email'";
-$result = mysqli_query($mysqli, $query) or die(mysqli_error());
+$result = mysqli_query($mysqli, $query) or die(mysqli_error($mysqli));
 $num_row = mysqli_num_rows($result);
 $row = mysqli_fetch_array($result);
 
